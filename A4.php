8a6989fe9fb8a631c3a369c518b4ac82cda61248
Dzmitry Tsimashenka <d.a.tsimashenka@godeltech.com>
2021-09-14 14:01:42 +0300

    improve task 4 performance

--- A4.php
+++ A4.php
@@ -5,30 +5,24 @@ class A {
      * @return Integer
      */
     function minSubarray($nums, $p) {
-        $tmpSum = 0;
-        $sum = 0;
-
-        foreach ($nums as &$num) {
-            $sum += $num;
-        }
-        $restToRemove = $sum % $p;
-
+        $restToRemove = array_sum($nums) % $p;
         if(0 === $restToRemove) {
             return 0;
         }
-
-        $map = [ 0 => -1];
+        $tmpRest = 0;
+        $map = [0 => -1];
         $result = PHP_INT_MAX;
-
-        foreach ($nums as $index => &$num) {
-            $tmpSum = ($tmpSum + $num) % $p;
-            $map[$tmpSum] = $index;
-            $rest = ($tmpSum - $restToRemove + $p) % $p;
-            if(isset($map[$rest]) && $map[$rest] != end($map)) {
-                $result = min($result, $index - $map[$rest]);
+        for ($i = 0; $i < count($nums); $i++) {
+            $tmpRest = ($tmpRest + $nums[$i]) % $p;
+            $map[$tmpRest] = $i;
+            $rest = ($tmpRest - $restToRemove + $p) % $p;
+            if(isset($map[$rest])) {
+                $result = min($result, $i - $map[$rest]);
             }
         }
 
         return $result >= count($nums) ? -1 : $result;
     }
 }
+
+var_dump((new A())->minSubarray([8,32,31,18,34,20,21,13,1,27,23,22,11,15,30,4,2],148));
