1ad22fbfb8eb708ceff7ba15f3678ca415e929ea
Dzmitry Tsimashenka <d.a.tsimashenka@godeltech.com>
2021-09-14 11:28:28 +0300

    improve task 3 performance

--- A3.php
+++ A3.php
@@ -1,34 +1,55 @@
 <?php
 
-class A {
-
-    function isPrime($n) {
+class A
+{
+    function isPrime(&$n)
+    {
         if ($n <= 1) return false;
         if ($n <= 3) return true;
-        if ($n % 2 == 0 || $n % 3 == 0) return false;
-        
-        for ($i = 3; $i <= sqrt($n); $i+=2){
-            if ($n % $i == 0) return false;     
+
+        if ($n < 25) {
+            $primes = [2, 3];
+        } else if ($n < 100){
+            $primes = [2, 3, 5, 7, 11, 13, 17];
+        } else if ($n < 1000) {
+            $primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+        } else  {
+            $primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
         }
+
+        foreach ($primes as &$prime) {
+            if ($n % $prime === 0) return false;
+        }
+
+        if ($n >= 10000) {
+            for ($i = 101; $i <= intval(sqrt($n)); $i+=2){
+                if ($n % $i == 0) return false;
+            }
+        }
+
         return true;
     }
 
-    function isPalindrome($x){
+    function isPalindrome($x)
+    {
         return $x == strrev($x);
     }
 
-    function getPrimePal($n) {
+    function getPrimePal($n)
+    {
         while (true) {
-            if (strlen($n) % 2 === 0 && $n > 11){
-                $n = pow(10, ceil(log10($n+1)));
+            if (strlen($n) % 2 === 0 && $n > 11) {
+                $n = intval(pow(10, ceil(log10($n + 1))));
                 continue;
             }
-            if (!$this->isPalindrome($n)) {
-                $n++;
-                continue;
+
+            if ($this->isPalindrome($n) && $this->isPrime($n)) {
+                return $n;
             }
-            if ($this->isPrime($n)) return $n;
+
             $n++;
         }
     }
 }
+
+var_dump((new A())->getPrimePal(51115));
